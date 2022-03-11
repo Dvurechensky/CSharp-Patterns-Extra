93694a1720afe874068e8dfc63356f9a5c0eed06
Dormammu <saomuun@mail.ru>
2022-03-11 14:14:39 +0300

    12

--- Pattern_1-Стратегия/Pattern_1-Адаптер(Adapter)/Program.cs
+++ Pattern_1-Стратегия/Pattern_1-Адаптер(Adapter)/Program.cs
@@ -4,14 +4,50 @@
  * Глава_12: Адаптер (Adapter)
  * 
  * - преобразует интерфейс одного класса в интерфейс другого, который 
- * - ожидают клиенты. Адаптер делает возможной совместную работу 
- * - классов с несовместимыми интерфейсами
+ *   ожидают клиенты. Адаптер делает возможной совместную работу 
+ *   классов с несовместимыми интерфейсами
  */
+
+class Motorcycle { }
+
+//Что адаптируем
+class Voskhod : Motorcycle 
+{
+    public void Sound() => Console.WriteLine("DRDRDR");
+}
+
+//Цель на которую нужно ориентироваться
+//при адаптации
+interface Isport
+{
+    void MakeNoise();
+}
+
+//Пример готового объекта
+//Адаптированного под цель
+class Honda : Motorcycle, Isport
+{
+    public void MakeNoise() => Console.WriteLine("hooondaaa");
+}
+
+//Адаптер
+//Адаптирует простой класс под цель
+class TuningVoskhod : Isport
+{
+    Voskhod moto;
+    public TuningVoskhod(Voskhod moto) => this.moto = moto;
+    public void MakeNoise()
+    {
+        Console.WriteLine("trsh");
+        moto.Sound();
+    }
+}
 
 class Program
 {
     public static void Main(string[] args)
     { 
-
+        var tun = new TuningVoskhod(new Voskhod());
+        tun.MakeNoise();
     }
 }
