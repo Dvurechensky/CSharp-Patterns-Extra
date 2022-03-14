7028985bde67bb1f972e5d0ec5a1ea441f26d378
Dormammu <saomuun@mail.ru>
2022-03-14 12:59:19 +0300

    4

---
+++ Pattern_1-Стратегия/Pattern_3-Декоратор(Decorator)/Program.cs
@@ -0,0 +1,138 @@
+﻿/*
+ * СТРУКТУРНЫЕ ПАТТЕРНЫ
+ * 
+ * Глава_13: Декоратор (Decorator)
+ * 
+ * -  динамически добавляет объекту новые обязанности. Является гибкой 
+ *    альтернативой порождению подклассов с целью расширения функциональности
+ */
+
+//Пример 1
+public sealed class Car
+{
+    public string Model { get; set; }
+    public Car(string model, IEnumerable<string> options)
+    {
+
+    }
+    public List<string> Options { get; protected set; }
+    public int GetPrice() { return 0; }
+    public void Move() { }
+    public void Stop() { }
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
+
+class MyCar
+{
+    protected Car baseCar;
+    public MyCar(string model, IEnumerable<string> options)
+    {
+        baseCar = new Car(model, options);
+    }
+    public List<string> Options { get => baseCar.Options; }
+    public int GetPrice() => baseCar.GetPrice() + 100;
+
+    public void Move() { baseCar.Move(); }
+    public void Stop() { baseCar.Stop(); }
+
+    public override string ToString()
+    {
+        return $"{baseCar?.ToString()} +++";
+    }
+}
+
+//Пример 2 (Динамический декоратор)
+class Pizza
+{
+    public virtual string MakePizza() => "Dough =>";
+}
+
+class ChickenPizza : Pizza
+{
+    Pizza pizza;
+    public ChickenPizza(Pizza pizza)
+    {
+        this.pizza = pizza;
+    }
+    public override string MakePizza()
+        => pizza.MakePizza() + "ChickenPizza =>";
+}
+
+class MeatPizza : Pizza
+{
+    Pizza pizza;
+    public MeatPizza(Pizza pizza)
+    {
+        this.pizza = pizza;
+    }
+    public override string MakePizza()
+        => pizza.MakePizza() + "MeatPizza =>";
+}
+
+class CheesePizza : Pizza
+{
+    Pizza pizza;
+    public CheesePizza(Pizza pizza)
+    {
+        this.pizza = pizza;
+    }
+    public override string MakePizza()
+        => pizza.MakePizza() + "CheesePizza =>";
+}
+
+//Пример 3 (статический декоратор - используем обобщение)
+public abstract class Pizza_Abstr
+{
+    public virtual string MakePizza() => string.Empty;
+}
+
+public class ChickPizza : Pizza_Abstr
+{
+    public ChickPizza() { }
+    public override string MakePizza() => "ChickPizza => ";
+}
+
+public class Pepper<T> : Pizza_Abstr
+    where T : Pizza_Abstr, new ()
+{
+    T pizza;
+    public Pepper() => pizza = new T();
+    public override string MakePizza()
+    {
+        return $"{pizza.MakePizza()} Pepper =>";
+    }
+}
+
+public class Olives<T> : Pizza_Abstr
+    where T : Pizza_Abstr, new()
+{
+    T pizza;
+    public Olives() => pizza = new T();
+    public override string MakePizza()
+    {
+        return $"{pizza.MakePizza()} Olives =>";
+    }
+}
+
+class Program
+{
+    public static void Main(string[] argv)
+    {
+        //2
+
+        Pizza chickenPizza = new ChickenPizza(new Pizza());
+        // Dough -> ChickenPizza ->
+        Console.WriteLine(chickenPizza.MakePizza());
+        var chickenCheesePizza = new CheesePizza(new ChickenPizza(new Pizza()));
+        Console.WriteLine(chickenCheesePizza.MakePizza());
+
+        //3
+        Pizza_Abstr pizza_Abstr = new ChickPizza();
+        Console.WriteLine(pizza_Abstr.MakePizza());
+        Pizza_Abstr chickenOlivesPizza = new Olives<ChickPizza>();
+        Console.WriteLine(chickenOlivesPizza.MakePizza());
+    }
+}
---
+++ Pattern_1-Стратегия/Pattern_4-Компоновщик(Composite)/Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * СТРУКТУРНЫЕ ПАТТЕРНЫ
+ * 
+ * Глава_14: Компоновщик (Composite)
+ * 
+ * -  компонует объекты в древовидные структуры для представления 
+ *    иерархий «часть — целое». Позволяет клиентам единообразно 
+ *    трактовать индивидуальные и составные объекты
+ */
+
+class UIElement
+{
+    public UIElement(string name) 
+    {
+        Children = new List<UIElement>();
+    }
+    public List<UIElement> Children { get; set; }
+    public string Name { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+    protected virtual string Draw(string p = "") { return p; }
+    public override string ToString() => $"{Draw()}";
+}
+
+class TextBox : UIElement
+{
+    public TextBox(string name = "textBox") : base(name) { }
+}
+
+class Label : UIElement
+{
+    public Label(string name = "label") : base(name) { }
+}
+
+class Panel : UIElement
+{
+    public Panel(string name = "panel") : base(name) { }
+}
+
+class TextBlock : UIElement
+{
+    public TextBlock(string name = "textBlock") : base(name) { }
+}
+
+public class Program
+{
+    public static void Main(string[] argv)
+    {
+        var panel1 = new Panel("panel1");
+        var tb1 = new TextBox("textBox1");
+        var tb2 = new TextBox("textBox2");
+        panel1.Children.Add(tb1);
+        var panel2 = new Panel("panel2");
+        var tb3 = new TextBox("textBox3");
+        var lbl1 = new Label("label1");
+        lbl1.Children.Add(new TextBlock("textBlock1"));
+        panel2.Children.Add(tb3);
+        panel2.Children.Add(lbl1);
+        panel1.Children.Add(panel2);
+        panel1.Children.Add(tb2);
+
+        Console.WriteLine(panel1);
+    }
+}
